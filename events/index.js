a66a84a1500c248934bbba56a88dffbba6b7481a
rizkiauliasa <rizkiauliasa@student.uns.ac.id>
2023-03-13 21:59:46 +0700

    6. Events

---
+++ events/index.js
@@ -0,0 +1,15 @@
+// TODO 1
+const { EventEmitter } = require('events');
+
+const birthdayEventListener = (name) => {
+    console.log(`Happy birthday ${name}!`);
+}
+
+// TODO 2
+const myEmitter = new EventEmitter();
+
+// TODO 3
+myEmitter.on('birthday', birthdayEventListener);
+
+// TODO 4
+myEmitter.emit('birthday','aul');
